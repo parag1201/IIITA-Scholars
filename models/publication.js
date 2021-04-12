22694ad33c7b89fc911179874f9e080945a58d7c
Bhavya Girotra <45626016+pseudocoder10@users.noreply.github.com>
2021-04-13 00:43:38 +0530

    updated models

---
+++ models/publication.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const PublicationSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    abstract: {
+        type: String,
+        reqired: true,
+    },
+    summary: {
+        type: String,
+        reqired: true,
+    },
+    googleScholarLink: {
+        type: String,
+        required: true,
+    },
+    publicationLink: {
+        type: String,
+        required: true,
+    },
+    authors: [],
+    keywords: [],
+    conference: {
+        type: String,
+    },
+    journal: {
+        type: String,
+    },
+    year: {
+        type: Date,
+        required: true,
+    },
+    citations: {
+        type: Number,
+        required: true,
+    },
+})
+
+module.exports = Publication = mongoose.model('publication', ProfSchema)
