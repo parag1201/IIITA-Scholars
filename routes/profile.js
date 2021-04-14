3eb34c9ffcfbd737949cd4820eb09ca5f9717334
Bhavya Girotra <45626016+pseudocoder10@users.noreply.github.com>
2021-04-14 20:26:16 +0530

    profile page

--- routes/profile.js
+++ routes/profile.js
@@ -22,14 +22,32 @@ router.get('/all', async (req, res) => {
 // @access   Public
 
 router.get('/:id', (req, res) => {
+    var profData
+    var articles = []
     Profile.findById(req.params.id)
         .lean()
         .then((result) => {
-            res.render('../views/profile', { profData: result })
+            profData = result
         })
         .catch((err) => {
             console.log(err)
         })
+
+    // Publication.find()
+    //     .lean()
+    //     .then((result) => {
+    //         result.forEach((article) => {
+    //             if (article.authors[0] == profData.name) {
+    //                 articles.push(article)
+    //             }
+    //         })
+    //     })
+    //     .catch((err) => {
+    //         console.log(err)
+    //     })
+
+    console.log(profData)
+    res.render('../views/profile', { profData, articles })
 })
 
 module.exports = router
