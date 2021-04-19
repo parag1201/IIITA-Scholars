aa6f4dec5fb12cddc7f87a46fc2020378dc237fd
parag1201 <goyalparag127@gmail.com>
2021-04-19 11:06:53 +0530

    something something....

--- routes/publication.js
+++ routes/publication.js
@@ -13,9 +13,41 @@ router.get("/", async (req, res) => {
 			return res.status(400).json({ msg: "No papers found" });
 		}
 		console.log(result);
-		res.render('../views/search.hbs', {papers: result});
+		res.render("../views/search.hbs", { papers: result });
 	} catch (error) {
 		console.error(error.mesage);
+		res.status(500).json({ msg: "Server Error" });
+	}
+});
+
+router.get("/search", async (req, res) => {
+	try {
+		const { year, tag, title, faculty } = req.query;
+
+		const result = await Publication.find().lean();
+		if (result.length === 0) {
+			return res.status(400).json({ msg: "No papers found" });
+		}
+		// console.log(result);
+		// console.log(year);
+		// console.log(typeof(year));
+		// if (year.length() > 0) {
+		// 	result = result.filter((p) => p.year.toString() === year);
+		// }
+
+		// if (tag) {
+		// 	result = result.filter((p) => {
+		// 		p.keywords.
+		// 	});
+		// }
+		// if (faculty) {
+		// }
+		// if (title) {
+		// }
+		console.log(result);
+		res.render("../views/search.hbs", { papers: result });
+	} catch (error) {
+		console.error(error.message);
 		res.status(500).json({ msg: "Server Error" });
 	}
 });
