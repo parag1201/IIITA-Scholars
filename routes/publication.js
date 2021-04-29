b0c42b3465bfdfd57b62b7cd850754f45fdfbd4c
Bhavya Girotra <45626016+pseudocoder10@users.noreply.github.com>
2021-04-29 14:58:06 +0530

    use downloaded images instead of links

--- routes/publication.js
+++ routes/publication.js
@@ -1,73 +1,72 @@
-const express = require("express");
-const router = express.Router();
-const Publication = require("../models/publication");
+const express = require('express')
+const router = express.Router()
+const Publication = require('../models/publication')
 
 // @route    GET /papers
 // @desc     get all papers
 // @access   Public
 
+router.get('/search', async (req, res) => {
+    try {
+        // Search for Authors
+        if (req.query.faculty) {
+            const regex = new RegExp(req.query.faculty, 'i')
 
-router.get("/search", async (req, res) => {
-	try {
-		// Search for Authors
-		if(req.query.faculty) {
-			const regex = new RegExp(req.query.faculty,'i');
+            Publication.find({ authors: { $in: regex } }, (err, docs) => {
+                res.render('../views/search.hbs', { papers: docs })
+            }).lean()
+        }
+        // Search by title
+        else if (req.query.name) {
+            const regex = new RegExp(req.query.name, 'i')
 
-			Publication.find({authors: {"$in": regex} }, (err, docs) => {
-				res.render('../views/search.hbs', {papers: docs})
-			}).lean();
-		}
-		// Search by title
-		else if(req.query.name) {
-			const regex = new RegExp(req.query.name,'i');
+            Publication.find({ title: regex }, (err, docs) => {
+                res.render('../views/search.hbs', { papers: docs })
+            }).lean()
+        } else if (req.query.year) {
+            Publication.find({ year: '2018' }, (err, docs) => {
+                res.render('../views/search.hbs', { papers: docs })
+            }).lean()
+        } else {
+            Publication.find((err, docs) => {
+                res.render('../views/search.hbs', { papers: docs })
+            }).lean()
+        }
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({ msg: 'Server Error' })
+    }
+})
 
-			Publication.find({title: regex }, (err, docs) => {
-				res.render('../views/search.hbs', {papers: docs})
-			}).lean();
-		}
-		else if(req.query.year) {
-			Publication.find({year: "2018" }, (err, docs) => {
-				res.render('../views/search.hbs', {papers: docs})
-			}).lean();
-		}
-		else {
-			Publication.find((err, docs) => {
-				res.render('../views/search.hbs', {papers: docs})
-			}).lean();
-		}
-		
-	} catch (error) {
-		console.error(error.message);
-		res.status(500).json({ msg: "Server Error" });
-	}
-});
+router.get('/', async (req, res) => {
+    try {
+        const result = await Publication.find().lean()
+        if (result.length === 0) {
+            return res.status(400).json({ msg: 'No papers found' })
+        }
 
-router.get("/", async (req, res) => {
-	try {
-		const result = await Publication.find().lean();
-		if (result.length === 0) {
-			return res.status(400).json({ msg: "No papers found" });
-		}
+        result.forEach((paper) => {
+            var list = []
+            for (
+                let index = 0;
+                index < Math.min(4, paper.authors.length);
+                index++
+            ) {
+                const author = paper.authors[index]
+                list.push(author)
+            }
+            paper.authors = list
+        })
 
-		result.forEach(paper => {
-			var list = [];
-			for (let index = 0; index < Math.min(4, paper.authors.length); index++) {
-				const author = paper.authors[index];
-				list.push(author);
-			}
-			paper.authors = list;
-			console.log(paper.authors.length);
-		});
-
-		res.render("../views/search.hbs", { papers: result});
-	} catch (error) {
-		console.error(error.mesage);
-		res.status(500).json({ msg: "Server Error" });
-	}
-});
+        res.render('../views/search.hbs', { papers: result })
+    } catch (error) {
+        console.error(error.mesage)
+        res.status(500).json({ msg: 'Server Error' })
+    }
+})
 
 function escapeRegex(text) {
-    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-}; 
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
+}
 
-module.exports = router;
+module.exports = router
